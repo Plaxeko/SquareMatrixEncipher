a959e82c8b27e01880343c7424984e0634f3eff3
Amir Torabi <Torabi10@gmail.com>
2017-04-05 02:46:57 -0700

    Cleaned up code and added comments

--- main.cpp
+++ main.cpp
@@ -11,98 +11,84 @@ char  randchar(void);
 int side_length(int length);
 void printArray();
 
+/*******************************************************************************
+*This program takes < * .txt as an argument and returns an enciphered
+*message. It parses the message through a row major 2d square matrix array and
+*then prints in a column major. The empty cells of the array are filled with
+*randchar and produces an enciphered message.
+*******************************************************************************/
+
 int main(int  argc, char *argv[], char **env)
 {
     int i = 0;
-   // int length = 0;
     string str  = "";
-
     int counter = 0;
 
-
-
-
+    //loop through each char of string to be enciphered.
     while ( ! cin.eof() )
     {
         char s = cin.get();
-       str += tolower(s);
-
-
-
+        str += tolower(s);
     }
 
-//    cout << str << endl;
-
+//check each character for isalpha and erase non-alpha
 for (int i = 0 ; i < str.length() ; ++i)
 {
-        if (!isalpha(str[i]))
-        {
-                str.erase(i,1);
-                --i;
-        }
+    if (!isalpha(str[i]))
+       {
+             str.erase(i,1);
+             --i;
+       }
 }
-
-//cout << str << endl;
 
 int length  = str.length();
 char arrayCounter[(side_length(length))][(side_length(length))];
 
-//cout << length << endl;
-//cout <<  side_length(length) << endl;
-
+       //create char matrix and place row major with  input string
        for (int r  = 0; r  < side_length(length); r++)
        {
             for (int c  = 0; c < side_length(length); c++)
             {
-
               arrayCounter[r][c] = str[counter++];
 
                    if (arrayCounter[r][c] == 0)
                    {
                        char letChar = tolower(randchar());
-                        arrayCounter[r][c] += letChar;
+                        arrayCounter[r][c] += letChar;// append empty array with randchar
                    }
-
-               cout << arrayCounter[r][c];
-
             }
-                cout << endl;
+
        }
 
        cout << endl;
 
+       //print column major enciphered message
        for(int a = 0; a < side_length(length); ++a)
-        for(int b = 0; b < side_length(length); ++b)
+           for(int b = 0; b < side_length(length); ++b)
        cout << arrayCounter[b][a];
-
-
-//cout << i << endl;
-
+       cout << endl;
 
 return 0;
 
 }
 
-
-
 int side_length(int length)
 {
-     int strlen = sqrt(length) + 1;
+
      // call side_length with the length of the message
      // it should return you an integer which is the
      // correct length of the square array of characters
      // to hold the message, plus any padding characters.
      // int n = sqrt (s.length()) + 1;
+     int strlen = sqrt(length) + 1;
      return strlen;
 
 }
 
+//return a random lower-case alphabetic character when calle
 char  randchar(void)
 {
-         //return a random lower-case alphabetic character when called
-        // note: (((rand() * some multiplier) % 26 )+ 'a') will produce such a character
-
-        int c = ' ' ;
+        int c = ' ';
         //while (c < 'a' || c > 'z')
         while (!isalpha(c))
         {
@@ -113,3 +99,6 @@ char  randchar(void)
         }
         return c ;
 }
+
+
+
